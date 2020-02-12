14aac8c90d1d12027323c4970abd9ac4f2cd1546
renan mascarenhas goncalves <renanmascg@gmail.com>
2020-02-12 20:42:34 -0300

    Refatora a nome da pasta que contem o arquivo network info, altera a biblioteca utilizada para uma que atende melhor as necessidades da aplicação, implementa o network info e realiza seus testes

--- lib/features/number_trivia/data/repositories/number_trivia_repository_impl.dart
+++ lib/features/number_trivia/data/repositories/number_trivia_repository_impl.dart
@@ -1,7 +1,7 @@
 import 'package:dartz/dartz.dart';
 import 'package:flutter_clean_architecture/features/core/error/exceptions.dart';
 import 'package:flutter_clean_architecture/features/core/error/failures.dart';
-import 'package:flutter_clean_architecture/features/core/platform/network_info.dart';
+import 'package:flutter_clean_architecture/features/core/network/network_info.dart';
 import 'package:flutter_clean_architecture/features/number_trivia/data/datasources/number_trivia_local_data_source.dart';
 import 'package:flutter_clean_architecture/features/number_trivia/data/datasources/number_trivia_remote_data_source.dart';
 import 'package:flutter_clean_architecture/features/number_trivia/domain/entities/number_trivia.dart';
